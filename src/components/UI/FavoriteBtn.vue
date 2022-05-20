f2bbb7ac20e41c0b52e73d3983416e7571b68a46
Maksym Solomianyi <maxssolom@gmail.com>
2022-05-20 20:51:37 +0300

    Add a custom button.

---
+++ src/components/UI/FavoriteBtn.vue
@@ -0,0 +1,22 @@
+<template>
+  <button
+      class="favorite-btn"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "FavoriteBtn"
+}
+</script>
+
+<style scoped>
+.favorite-btn {
+  cursor: pointer;
+  border: none;
+  background: transparent;
+  margin-left: 20px;
+}
+</style>
